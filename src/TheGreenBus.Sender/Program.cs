8757c6d5d0b40684d7e0e8936ba9d72eef04bbb8
michaeldeongreen <michaeldeongreen@gmail.com>
2017-07-11 17:56:43 -0500

    *First check-in.  Basic send complete

---
+++ src/TheGreenBus.Sender/Program.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.ServiceBus.Messaging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TheGreenBus.Sender
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string connectionString = "Endpoint=sb://thegreenbus.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=9os+TCEaZ/AjUXk3ZWJoAw944CJrcFIBOyCdLgX1XR8=";
+            string queuename = "queuedemo";
+
+            var client = QueueClient.CreateFromConnectionString(connectionString, queuename);
+            var message = new BrokeredMessage("I'm on The Green Bus!");
+            client.Send(message);
+
+            Console.WriteLine("Message was successfully sent!");
+
+            Console.ReadLine();
+        }
+    }
+}
